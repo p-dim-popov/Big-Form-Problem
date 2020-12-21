406a7e9df28f019b81a4e29b704a1fedd0cb1bfc
Petar Popov <p.dim.popov@gmail.com>
2020-12-21 15:44:42 +0200

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import BigForm from "./BigForm";
+
+export default function App() {
+    return (
+        <div className="App">
+            <BigForm />
+        </div>
+    );
+}
